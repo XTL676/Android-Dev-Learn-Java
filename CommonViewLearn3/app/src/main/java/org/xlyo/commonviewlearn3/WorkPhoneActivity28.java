d44f64958494c35c11668c65d11242899d9c186d
XTL676 <tianluoqaq@qq.com>
2024-09-24 17:46:50 +0800

    [常用控件学习 - 作业] 手机返回提示28

---
+++ CommonViewLearn3/app/src/main/java/org/xlyo/commonviewlearn3/WorkPhoneActivity28.java
@@ -0,0 +1,35 @@
+package org.xlyo.commonviewlearn3;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WorkPhoneActivity28 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_work_phone28);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Toast.makeText(this, "退出本界面", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}

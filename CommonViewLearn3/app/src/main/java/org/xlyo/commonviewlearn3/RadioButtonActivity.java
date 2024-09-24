80e2f3d02515f7aea43cf66e0b7c915aa9ea0112
XTL676 <tianluoqaq@qq.com>
2024-09-24 16:04:36 +0800

    [常用控件学习] 单选按钮控件

---
+++ CommonViewLearn3/app/src/main/java/org/xlyo/commonviewlearn3/RadioButtonActivity.java
@@ -0,0 +1,33 @@
+package org.xlyo.commonviewlearn3;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RadioButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_radio_button);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener((group, value) -> {
+            TextView textView = findViewById(R.id.textView2);
+            textView.setText("♂♀：" + ((RadioButton) findViewById(group.getCheckedRadioButtonId())).getText());
+        });
+    }
+}
